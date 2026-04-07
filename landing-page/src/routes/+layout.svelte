9c85561d1ba04c0fb6ac0546585bbd1e5ae27d0b
infraflakes <infraflakes@proton.me>
2026-04-07 22:37:44 +0700

    Add favicon

--- landing-page/src/routes/+layout.svelte
+++ landing-page/src/routes/+layout.svelte
@@ -1,9 +1,14 @@
 <script lang="ts">
 import '../app.css';
 import Nav from '$lib/components/Nav.svelte';
+import favicon from '$lib/assets/favicon-32x32.png';
 
 let { children } = $props();
 </script>
+
+<svelte:head>
+  <link rel="icon" href={favicon} />
+</svelte:head>
 
 <Nav />
 <main class="pt-16">
